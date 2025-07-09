5e3f8b126a334ecc11e590113922546395b3d252
Leandro Marques <leandrojerse@gmail.com>
2025-07-09 14:31:15 -0300

    Spring security checklist, not tested

---
+++ password-grant/spring-boot-3-4-3-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
@@ -0,0 +1,36 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.entities.Role;
+import com.devsuperior.demo.entities.User;
+import com.devsuperior.demo.projections.UserDetailsProjection;
+import com.devsuperior.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(email);
+        if (result.size() == 0 ) {
+            throw new UsernameNotFoundException("username not found");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection userDetailsProjection : result) {
+            user.addRole(new Role(userDetailsProjection.getRoleId(), userDetailsProjection.getUsername() ));
+        }
+        return user;
+    }
+}
